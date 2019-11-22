28300f436e81ce3d6dfcb6d3b4b506ad30a26d50
Simon Wicky <simon.wicky@epfl.ch>
2019-11-22 11:06:50 +0100

    remove projdyn

--- ProjDyn/cmake/FindTetGen.cmake
+++
@@ -1,4 +0,0 @@
-set(TETGEN_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/externals/tetgen")
-include_directories(${TETGEN_INCLUDE_DIR})
-add_subdirectory(${TETGEN_INCLUDE_DIR})
-target_link_libraries(tetgen ${LIBRARIES})
